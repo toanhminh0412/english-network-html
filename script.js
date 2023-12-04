ccfce9c9c0baff1f3e9135ee0de9c50d2b792b63
Archie To <toanhminh0412@gmail.com>
2023-12-04 11:02:39 -0800

    Revision 2

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.onload = () => {
     
     // Close card when clicking the card's "X" button inside of relationship modal
     $('.card-btn-close').on('click', function() {
-        const personCard = $(this).closest('.card');
-        personCard.hide();
+        console.log('Clicked on close btn');
+        const card = $(this).closest('.card');
+        card.hide();
     });
 
     // Render all cards when opening a relationship modal
@@ -17,5 +18,36 @@ window.onload = () => {
     $(".letter-open-button").on('click', function() {
         const comment = $(this).data('comment');
         $(this).closest('.modal-body').find('.letter-comment').attr("src", comment);
+    });
+
+    // Zoom in to the graph
+    let minX = 0;
+    let minY = 0;
+    let width = 1500;
+    let height = 500;
+    let widthStep = 100;        // Amount of width change when zoom in/out
+    let heightStep = 7/10 * widthStep;       // Amount of height change when zoom in/out
+    $('#zoom-in-btn').on('click', function() {
+        // Prevent zooming in too much
+        if (width === 900) {
+            return;
+        }
+        width -= widthStep;
+        height -= heightStep;
+        minX += widthStep / 2;
+        minY += heightStep / 2;
+        document.getElementById('container').setAttribute('viewBox', `${minX} ${minY} ${width} ${height}`);
+    });
+
+    $('#zoom-out-btn').on('click', function() {
+        // Prevent zooming out too much
+        if (width === 2000) {
+            return;
+        }
+        width += widthStep;
+        height += heightStep;
+        minX -= widthStep / 2;
+        minY -= heightStep / 2;
+        document.getElementById('container').setAttribute('viewBox', `${minX} ${minY} ${width} ${height}`);
     });
 };
