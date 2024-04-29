61150591cd741211d7827fecfbcd28e4094acb01
Minh To <toanhminh0412@gmail.com>
2024-04-28 22:09:47 -0700

    Reformat code

--- dragandzoom.js
+++ dragandzoom.js
@@ -7,31 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
     var zoomFactor = 1.1;  // Defines how much to zoom on each button click or scroll
   
     svg.addEventListener('mousedown', function(e) {
-      isDragging = true;
-      lastX = e.clientX;
-      lastY = e.clientY;
-      svg.style.cursor = 'grabbing';
+        isDragging = true;
+        lastX = e.clientX;
+        lastY = e.clientY;
+        svg.style.cursor = 'grabbing';
     });
   
     window.addEventListener('mousemove', function(e) {
-      if (isDragging) {
-        var deltaX = (e.clientX - lastX) / currentZoom;
-        var deltaY = (e.clientY - lastY) / currentZoom;
-        updateViewBox(deltaX, deltaY);
-        lastX = e.clientX;
-        lastY = e.clientY;
-      }
+        if (isDragging) {
+            var deltaX = (e.clientX - lastX) / currentZoom;
+            var deltaY = (e.clientY - lastY) / currentZoom;
+            updateViewBox(deltaX, deltaY);
+            lastX = e.clientX;
+            lastY = e.clientY;
+        }
     });
   
     window.addEventListener('mouseup', function(e) {
-      isDragging = false;
-      svg.style.cursor = 'grab';
+        isDragging = false;
+        svg.style.cursor = 'grab';
     });
   
     svg.addEventListener('wheel', function(e) {
-      e.preventDefault();  // Prevent the page from scrolling
-      var direction = e.deltaY > 0 ? 1 : -1;
-      adjustZoom(direction);
+        e.preventDefault();  // Prevent the page from scrolling
+        var direction = e.deltaY > 0 ? 1 : -1;
+        adjustZoom(direction);
     });
   
     let zoomInButton = document.getElementById('zoom-in-btn');
@@ -45,27 +45,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function adjustZoom(direction) {
-      var viewBox = svg.getAttribute('viewBox').split(' ').map(Number);
-      var width = viewBox[2];
-      var height = viewBox[3];
-      var newWidth = width * Math.pow(zoomFactor, direction);
-      var newHeight = height * Math.pow(zoomFactor, direction);
+        var viewBox = svg.getAttribute('viewBox').split(' ').map(Number);
+        var width = viewBox[2];
+        var height = viewBox[3];
+        var newWidth = width * Math.pow(zoomFactor, direction);
+        var newHeight = height * Math.pow(zoomFactor, direction);
   
-      // Calculate new x and y to keep the zoom centered
-      viewBox[0] += (width - newWidth) / 2;
-      viewBox[1] += (height - newHeight) / 2;
-      viewBox[2] = newWidth;
-      viewBox[3] = newHeight;
+        // Calculate new x and y to keep the zoom centered
+        viewBox[0] += (width - newWidth) / 2;
+        viewBox[1] += (height - newHeight) / 2;
+        viewBox[2] = newWidth;
+        viewBox[3] = newHeight;
   
-      currentZoom *= Math.pow(zoomFactor, -direction);
-      svg.setAttribute('viewBox', viewBox.join(' '));
+        currentZoom *= Math.pow(zoomFactor, -direction);
+        svg.setAttribute('viewBox', viewBox.join(' '));
     }
   
     function updateViewBox(deltaX, deltaY) {
-      var viewBox = svg.getAttribute('viewBox').split(' ').map(Number);
-      viewBox[0] -= deltaX;
-      viewBox[1] -= deltaY;
-      svg.setAttribute('viewBox', viewBox.join(' '));
+        var viewBox = svg.getAttribute('viewBox').split(' ').map(Number);
+        viewBox[0] -= deltaX;
+        viewBox[1] -= deltaY;
+        svg.setAttribute('viewBox', viewBox.join(' '));
     }
   });
   
